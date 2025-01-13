3a70140413d66bff968be592f4e780fd9642581c
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2025-01-13 15:43:09 -0500

    Print book titles to html

--- main.js
+++ main.js
@@ -43,14 +43,12 @@ function addBookToLibrary() {
 
 function createBookCard(book) {
     // create book card html from object data
-    let htmlString = `
-    <div class="card">
-        <p class="title">${book.title}</p>
-        <p class="author">${book.author}</p>
-        <p class="pages">${book.pages}</p>
-        <p class="read">${book.read}</p>
-    </div>
-    `;
+    let bookCard = document.createElement('div');
+    
+    let title = document.createElement('p'); 
+    title.textContent = book.title;
+    
+    bookCard.appendChild(title);
 
     // <div class="card">
     // <p class="title">title</p>
@@ -59,11 +57,13 @@ function createBookCard(book) {
     // <p class="read">yes/no</p>
     // </div>
 
-    return htmlString;
+    return bookCard;
 }
 
 function displayBookCard(book) {
     // add book card's html to DOM
-    console.log(createBookCard(book));
-
+    console.log(createBookCard(book).innerHTML);
+    const container = document.querySelector('#container');
+    const currentBook = createBookCard(book)
+    container.appendChild(currentBook);
 }
