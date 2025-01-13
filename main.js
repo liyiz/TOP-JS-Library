ec61b0b73def3611fa7ffa24212024c04b2989c2
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2025-01-13 14:59:31 -0500

    Add object properties in constructor

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const myLibrary = [];
 
-function Book() {
+function Book(author, title, pages, read) {
   // the constructor...
+  this.author = author; // string
+  this.title = title; // string
+  this.pages = pages; // number
+  this.read = read; // boolean
 }
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+function createBookCard() {
+
+}
+
+function displayBookCard() {
+    // add book card's html to DOM
+
+    // <div class="card">
+    // <p class="title">title</p>
+    // <p class="author">author</p>
+    // <p class="pages">pages</p>
+    // <p class="read">yes/no</p>
+    // </div>
+
 }
