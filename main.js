0e34f2bcfc1caecd2789eff80034d7b3d1cff38c
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2025-01-18 13:32:53 -0500

    Rework read and delete buttons

--- main.js
+++ main.js
@@ -152,14 +152,27 @@ function createBookCard(book) {
     checkmark.addEventListener('click', () => {
         console.log("Time to toggle!");
         book.toggleRead();
+    });
+
+    let deletebtn = document.createElement('div');
+    deletebtn.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete-circle-outline</title><path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4M16,10V17A1,1 0 0,1 15,18H9A1,1 0 0,1 8,17V10H16M13.5,6L14.5,7H17V9H7V7H9.5L10.5,6H13.5Z" /></svg>';
+    deletebtn.classList.add('deletebtn');
+    deletebtn.addEventListener('click', () => {
+        console.log("Delete button clicked!");
+        // TODO trigger function to delete book - consider adding to Book prototype
     })
+
+    let actionbtns = document.createElement('div');
+    actionbtns.classList.add('actionbtns');
+    actionbtns.appendChild(checkmark);
+    actionbtns.appendChild(deletebtn);
 
     bookCard.appendChild(details);
     bookCard.classList.add('card');
 
     // book.read ? bookCard.classList.add('finished') : bookCard.classList.add('unfinished');
     bookCard.classList.add(book.read ? 'finished' : 'unfinished');
-    bookCard.appendChild(checkmark);
+    bookCard.appendChild(actionbtns);
     
 
     // <div class="card unfinished">
